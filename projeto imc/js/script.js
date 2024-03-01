5bb77c75b993f2bb927117a57e924495745309c6
amandmafra <135349875+amandmafra@users.noreply.github.com>
2024-03-01 19:46:43 -0300

    Add files via upload

---
+++ projeto imc/js/script.js
@@ -0,0 +1,37 @@
+import { Modal } from "./modal.js"
+import { AlertError } from "./alert-error.js"
+import { IMC, notANumber } from "./utils.js"
+
+/*Variáveis*/
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+form.onsubmit = function(event) {
+  event.preventDefault()
+  
+  const weight = inputWeight.value
+  const height = inputHeight.value
+
+  const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
+
+  if(weightOrHeightIsNotANumber) {
+    AlertError.open()
+    return;
+  }
+
+  AlertError.close()
+  
+  const result = IMC(weight, height)
+  const message = (`Seu IMC é de ${result}`)
+
+  Modal.message.innerText = message
+  Modal.open()
+}
+
+inputWeight.oninput = function() {
+  AlertError.close()
+}
+inputHeight.oninput = function() {
+  AlertError.close()
+}
